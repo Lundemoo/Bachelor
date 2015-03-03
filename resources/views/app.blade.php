2be80c8a666f5c42d620bf7e6ce9f74eb8f4c51d
christmw <7040.cw@gmail.com>
2015-03-03 10:31:36 +0100

    småting med css

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,9 +6,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Jara Bygg</title>
 
-	<link href="/css/app.css" rel="stylesheet">
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	<link href="css/app.css" rel="stylesheet">
+
 	<!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
 
@@ -43,13 +42,13 @@
 </div>
 				<ul class="nav navbar-nav navbar-right">
 					@if (Auth::guest())
-						<li><a href="/auth/login">Logg inn</a></li>
-						<li><a href="/auth/register">Registrer</a></li>
+						<li><a href="auth/login">Logg inn</a></li>
+						<li><a href="auth/register">Registrer</a></li>
 					@else
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->firstname }} {{ Auth::user()->lastname }} <span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
-								<li><a href="/auth/logout">Logg ut</a></li>
+								<li><a href="auth/logout">Logg ut</a></li>
 							</ul>
 						</li>
 					@endif
