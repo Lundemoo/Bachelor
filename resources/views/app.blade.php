a02f06fb929b590ce2a7c1859cda44315d679779
christmw <7040.cw@gmail.com>
2015-03-10 10:39:58 +0100

    endringer med layout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -21,44 +21,11 @@
 	<![endif]-->
 </head>
 <body>
-    
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-					<span class="sr-only">Toggle Navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="#">Logo?</a>
-			</div>
+<header> @include('header')</header>
 
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav">
-					<li><a href="/">Hovedmeny</a></li>
-				</ul>
-<div>
+	<div class="contents">@yield('content')</div>
 
-</div>
-				<ul class="nav navbar-nav navbar-right">
-					@if (Auth::guest())
-						<li><a href="auth/login">Logg inn</a></li>
-						<li><a href="auth/register">Registrer</a></li>
-					@else
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->firstname }} {{ Auth::user()->lastname }} <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="auth/logout">Logg ut</a></li>
-							</ul>
-						</li>
-					@endif
-				</ul>
-			</div>
-		</div>
-	</nav>
-
-	@yield('content')
+<footer> @include('footer')</footer>
 
 	<!-- Scripts -->
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
