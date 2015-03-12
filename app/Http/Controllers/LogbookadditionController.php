f005f4f17e5fb86692a1914b5d6d4059f2b7e045
Rune <runekj@student.hist.no>
2015-03-12 10:32:04 +0100

    logbookaddition med ruteberegning

---
+++ app/Http/Controllers/LogbookadditionController.php
@@ -0,0 +1,53 @@
+<?php namespace App\Http\Controllers;
+
+use App\Logbookaddition;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Auth;
+use App\User;
+use App\Project;
+use Request;
+use DB;
+use Carbon\Carbon;
+
+
+
+
+class LogbookadditionController extends Controller
+{
+
+    public function index()
+    {
+        $logbookadditions = DB::table('logbookaddition')->get();
+
+
+        return view('logbookaddition.index',['logbookadditions'=> $logbookadditions]);
+
+    }
+
+    public function create(){
+
+        return view('logbookaddition.create');
+
+
+    }
+
+    public function store(){
+
+        $input = Request::all();
+        $input['employeeNr'] = 1;
+        $input['totalkm'] = 100;
+        Logbookaddition::create($input);
+
+
+        $logbookadditions = DB::table('logbookaddition')->get();
+        return view('logbookaddition.index', ['logbookadditions' => $logbookadditions]);
+
+
+
+
+    }
+
+
+
+}
