978bd601bef79f050152b8a8b5995c0ce423c71c
root <petter_lundemo@hotmail.com>
2015-04-20 11:04:52 +0200

    Fungerende prosjektsider

--- app/Projectcontactpersons.php
+++ app/Projectcontactpersons.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 class Projectcontactpersons extends Model {
 
 
-        protected $table = "projectcontactpersons";
+        protected $table = "projectContactpersons";
 
 	protected $fillable = [
         'projectID',
