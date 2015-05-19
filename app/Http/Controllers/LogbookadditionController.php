1ab063ee40eb5deb3d35d1d2ece568f675fb3597
Rune <runekj@student.hist.no>
2015-05-19 10:28:28 +0200

    liste med prosjekter i reg kjørebok fiks

--- app/Http/Controllers/LogbookadditionController.php
+++ app/Http/Controllers/LogbookadditionController.php
@@ -78,8 +78,9 @@ class LogbookadditionController extends Controller
 
 
         $cars = Car::lists('nickname', 'registrationNR');
+        $projects = Project::lists('projectName', 'projectID');
 
-        return view('logbookaddition.create', array('cars' => $cars));
+        return view('logbookaddition.create', array('cars' => $cars, 'projects' => $projects));
 
 
     }
